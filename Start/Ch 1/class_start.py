29a0b754835859116eb88b6c742cd19599e214a9
Georgios Thaniotis <gthaniotis@gmail.com>
2025-01-06 09:58:44 +0000

    Updated Ch1

--- Start/Ch 1/class_start.py
+++ Start/Ch 1/class_start.py
@@ -4,26 +4,52 @@
 
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
 
     # TODO: double-underscore properties are hidden from other classes
-
+    __booklist = None
+    
     # TODO: create a class method
+    @classmethod
+    def getbooktypes(cls):
+        return cls.BOOK_TYPES
 
     # TODO: create a static method
+    @staticmethod
+    def getbooklist():
+        if Book.__booklist == None:
+            Book.__booklist = []
+        return Book.__booklist
 
+    # define a string representation for the Book objects
+    def __repr__(self):
+        return f"Book(title={self.title}, booktype={self.booktype})"
+    
     # instance methods receive a specific object instance as an argument
     # and operate on data specific to that object instance
     def set_title(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
-
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError(f"{booktype} is not a valid book type")
+        else:
+            self.booktype = booktype
 
 # TODO: access the class attribute
-
+print(f"Book types: {Book.getbooktypes()}")
 
 # TODO: Create some book instances
-
+b1 = Book("Title 1", "HARDCOVER")
+b2 = Book("Title 2", "PAPERBACK")
+# b3 = Book("Title 3", "COMIC")
 
 # TODO: Use the static method to access a singleton object
+thebooks = Book.getbooklist()
+thebooks.append(b1)
+thebooks.append(b2)
+print(thebooks)
+
+# Access the double-underscore property using the mangled name
+print(b1._Book__booklist)
--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -9,17 +9,32 @@ class Book:
         self.title = title
         # TODO: add properties
         self.author = author
-    # TODO: create instance methods
+        self.pages = pages
+        self.price = price
+        self.__secret = "This is a secret attribute"
 
+    # TODO: create instance methods
+    def getPrice(self):
+        if hasattr(self, "_discount"):
+            return self.price * (1 - self._discount)
+        else:
+            return self.price
+
+    def setDiscount(self, amount):
+        self._discount = amount
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
 
-# TODO: print the price of book1
+# # TODO: print the price of book1
+# print(b1.getPrice())
 
-
-# TODO: try setting the discount
-
+# # TODO: try setting the discount
+# print(b2.getPrice())
+# b2.setDiscount(0.25)
+# print(b2.getPrice())
 
 # TODO: properties with double underscores are hidden by the interpreter
+# print(b2.__secret)
+print(b2._Book__secret)
